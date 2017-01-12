9cbdb30bf23aa627faad9372dea09325f6b4c033
niyueming <niyueming@163.com>
2017-01-12 11:22:41 +0800

    子模块

--- app/src/main/java/net/nym/permissionuse/MainActivity.java
+++ app/src/main/java/net/nym/permissionuse/MainActivity.java
@@ -3,11 +3,19 @@ package net.nym.permissionuse;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import net.nym.permissionlibrary.activity.NPermissionActivity;
+import net.nym.permissionlibrary.permission.NPermission;
+
+public class MainActivity extends NPermissionActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new NPermission.Builder(this)
+                .requestCode(REQUEST_CAMERA)
+                .permissions(PERMISSIONS_CAMERA)
+                .build()
+                .request();
     }
 }
